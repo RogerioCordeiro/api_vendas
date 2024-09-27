162b60e64f0e8a07d44e7bd92af13b832dd74ea7
Rogerio Cordeiro <rocordeirosilva@gmail.com>
2024-09-27 10:30:29 -0300

    feat(services): mail template handlebars - test

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -11,15 +11,11 @@ class SendForgotPasswordEmailService {
   public async execute({ email }: IRequest): Promise<void> {
     const usersRepository = getCustomRepository(UsersRepository)
     const userTokensRepository = getCustomRepository(UserTokensRepository)
-
     const user = await usersRepository.findByEmail(email)
-
     if (!user) {
       throw new AppError('User does not exists.')
     }
-
     const { token } = await userTokensRepository.generate(user.id)
-
     await EtherealMail.sendMail({
       to: {
         name: user.name,
